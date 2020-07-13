ae248f299a14249aa9e4154fbc7aaac1a1eef8ad
srinivassankoji <srinivaschary.chary@gmail.com>
2020-07-13 10:21:07 +0530

    Swagger

---
+++ swggaer-yaml/src/test/java/com/songrgg/example/SwggaerYamlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.songrgg.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwggaerYamlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
